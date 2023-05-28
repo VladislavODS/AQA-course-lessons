ee3ae4600fb989ef21784684ea2d2e0217c3711e
Vlad <vladnigreev@gmail.com>
2023-05-28 15:58:31 +0300

    selector for accepting cookies added

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -8,6 +8,7 @@ public class SearchTests {
     @Test
     void successfulSearchTest() {
         open("https://www.google.com/");
+        $("[id=L2AGLb]").click();
         $("[name=q]").setValue("selenide").pressEnter();
         $("[id=search]").shouldHave(text("https://selenide.org"));
     }
